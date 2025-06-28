18bbee86d13bd3cf0615e7d18792177d7babf2b7
Alfonso Madrid <alfonso.madrid.dev@gmail.com>
2025-06-28 20:56:26 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,32 +1,37 @@
- // Cierra el menú móvil al hacer clic en un enlace
- document.querySelectorAll('.nav-links a').forEach(link => {
-  link.addEventListener('click', () => {
-    const navLinks = document.querySelector('.nav-links');
-    if (navLinks.classList.contains('open')) {
-      navLinks.classList.remove('open');
-    }
+  // Cierra el menú móvil al hacer clic en un enlace
+  document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('.nav-links a').forEach(link => {
+      link.addEventListener('click', () => {
+        console.log('detect click');
+        document.querySelector('.nav-links').classList.remove('open');
+      });
+    });
+  })
+
+  // Scrollspy: resalta el enlace activo según el scroll
+  const sections = document.querySelectorAll("section[id]");
+  const navItems = document.querySelectorAll(".nav-links a");
+
+  window.addEventListener("scroll", () => {
+    let current = "";
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(section => {
+      const sectionTop = section.offsetTop - 100;
+      if (scrollY >= sectionTop) {
+        current = section.getAttribute("id");
+      }
+    });
+
+    navItems.forEach(link => {
+      link.classList.remove("active");
+      if (link.getAttribute("href") === `#${current}`) {
+        link.classList.add("active");
+      }
+    });
   });
-});
 
-// Scrollspy: resalta el enlace activo según el scroll
-const sections = document.querySelectorAll("section[id]");
-const navItems = document.querySelectorAll(".nav-links a");
-
-window.addEventListener("scroll", () => {
-  let current = "";
-  const scrollY = window.pageYOffset;
-
-  sections.forEach(section => {
-    const sectionTop = section.offsetTop - 100;
-    if (scrollY >= sectionTop) {
-      current = section.getAttribute("id");
-    }
-  });
-
-  navItems.forEach(link => {
-    link.classList.remove("active");
-    if (link.getAttribute("href") === `#${current}`) {
-      link.classList.add("active");
-    }
-  });
+window.addEventListener('scroll', function () {
+  const hero = document.querySelector('.hero');
+  hero.style.opacity = 1 - window.scrollY / 500;
 });
